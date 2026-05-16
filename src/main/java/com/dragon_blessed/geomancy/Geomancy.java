fb547e5e3d36acb76fb0fa1b1e36f49ef6bd238a
LuketMeyer <158439787+LuketMeyer@users.noreply.github.com>
2026-05-16 16:39:29 -0400

    Added two new dummy items

--- src/main/java/com/dragon_blessed/geomancy/Geomancy.java
+++ src/main/java/com/dragon_blessed/geomancy/Geomancy.java
@@ -2,6 +2,7 @@ package com.dragon_blessed.geomancy;
 
 import org.slf4j.Logger;
 
+import com.dragon_blessed.geomancy.item.ModItems;
 import com.mojang.logging.LogUtils;
 
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -53,6 +54,9 @@ public class Geomancy {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -66,6 +70,11 @@ public class Geomancy {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+
+        if( event.getTabKey() == CreativeModeTabs.INGREDIENTS ) {
+            event.accept(ModItems.DOODAD);
+            event.accept(ModItems.SPARKING_DOODAD);
+        }
 
     }
 
---
+++ src/main/java/com/dragon_blessed/geomancy/item/ModItems.java
@@ -0,0 +1,28 @@
+package com.dragon_blessed.geomancy.item;
+
+import com.dragon_blessed.geomancy.Geomancy;
+
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems( Geomancy.MODID );
+
+    //where items are declared, java convention has their member (DOODAD) be in full caps,
+    //internal item name, doodad, needs to be all lowercase
+    public static final DeferredItem<Item> DOODAD = ITEMS.register( "doodad", 
+        () -> new Item(new Item.Properties() )
+    );
+
+    public static final DeferredItem<Item> SPARKING_DOODAD = ITEMS.register( "sparking_doodad", 
+        () -> new Item(new Item.Properties() )
+    );
+
+    public static void register(IEventBus eventBus ) {
+        ITEMS.register( eventBus );
+    }
+
+}
