f9e10de8cf65423c68804b317b9ae6244cfe97fc
LuketMeyer <158439787+LuketMeyer@users.noreply.github.com>
2026-05-16 20:28:50 -0400

    Update ModBlocks.java

--- src/main/java/com/dragon_blessed/geomancy/block/ModBlocks.java
+++ src/main/java/com/dragon_blessed/geomancy/block/ModBlocks.java
@@ -1,5 +1,16 @@
 package com.dragon_blessed.geomancy.block;
 
+import com.dragon_blessed.geomancy.Geomancy;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
 public class ModBlocks {
 
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks( Geomancy.MODID );
+
+    public static void register( IEventBus eventBus ) {
+        BLOCKS.register(eventBus);
+    }
+    
 }
